035d4447778dd880fa619001aad569ea3b2815a0
Leongoss <gossleon4@gmail.com>
2026-09-03 18:45:16 +0200

    Delete GOSS_LEON/about_me.py

--- GOSS_LEON/about_me.py
+++
@@ -1,9 +0,0 @@
-name = input("what is your name? ")
-age = input("how old are you? ")
-town = input("what is your home town? ")
-
-print("name:", name)
-print("age:", age)
-print("town: ", town)
-#end specifies the character printed at the end of the print() output
-#sep sets the character that separates values in print()
