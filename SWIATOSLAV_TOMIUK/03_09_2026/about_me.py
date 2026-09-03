8ce67610e1b5cbce2790f6e54768ee69c4301a32
PLANETA9091 <PLANETA9091@users.noreply.github.com>
2026-09-03 19:59:07 +0200

    Done exercise

---
+++ SWIATOSLAV_TOMIUK/03_09_2026/about_me.py
@@ -0,0 +1,9 @@
+name = input("Whats your name?\n")
+age = input("Whats your age?\n")
+homeTown = input("Whats your home town?\n")
+
+print("Name:", name)
+print("Age:", age)
+print("Home Town:", homeTown)
+# end parametr do specify what will be printed at the end, by default there is "/n"
+# sep parametr do specify what will be sepperating objects
