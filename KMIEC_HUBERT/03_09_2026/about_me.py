226832e6a0c960642b10cd28766ccd33af0e4310
Hubbercios3309 <hubbercios3309@wp.pl>
2026-09-03 17:30:52 +0200

    Create about_me.py

---
+++ KMIEC_HUBERT/03_09_2026/about_me.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+Home_town = input("Where are you from? ")
+
+print("Name:",name)
+print("Age:",age)
+print("Home town:",home_town)
+#end specifies what should be printed at the end of an output
+#sep specifies the separator between multiple values in a single print() call
