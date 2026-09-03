707b649e00532e7654b36d2f614157ffe44cc0da
Szefniebobas2 <pawel.m.cygan@gmail.com>
2026-09-03 19:00:27 +0200

    Update about_me.py

--- CYGAN_PAWEL/03_09_2026/about_me.py
+++ CYGAN_PAWEL/03_09_2026/about_me.py
@@ -1,1 +1,14 @@
+print("Please answer the following questions.")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+HomeTown = input("Enter your home town: ")
+# gathers the information from the user
+print()
+# makes space between the input and output lines
+print("Name:",name)
+print("Age:",age)
+print("Home town:",HomeTown)
+# prints out the gathered info
 
+# the end parameter specifies the character which is printed at the end of the print() function output
+# the sep parameter sets the character that separates values in the print() function
