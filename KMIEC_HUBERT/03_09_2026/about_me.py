d80ac0a43691bf066bffcc5ae782860f27c60cfd
Hubbercios3309 <hubbercios3309@wp.pl>
2026-09-03 17:38:35 +0200

    Update about_me.py

--- KMIEC_HUBERT/03_09_2026/about_me.py
+++ KMIEC_HUBERT/03_09_2026/about_me.py
@@ -5,5 +5,5 @@ Home_town = input("Where are you from? ")
 print("Name:",name)
 print("Age:",age)
 print("Home town:",home_town)
-#end specifies what should be printed at the end of an output
+#end specifies the character printed at the end of the print() output
 #sep specifies the separator between multiple values in a single print() call
