ed2e52638b027f4f5f38357edace1632d6e79166
Sebsiakk <11sebastianw@gmail.com>
2026-09-03 20:16:10 +0200

    Add comments explaining print function usage
    Added comments to clarify print function parameters.

---
+++ WALUSIAK_SEBASTIAN/03_09_202603_09_2026/about_me.py
@@ -0,0 +1,12 @@
+#The program asks you for your name,age and hometown and then prints it
+
+name = input("Whats your name?")
+age = input("Whats your age")
+homeTown = input("Whats your home town?")
+
+print("Name:", name)
+print("Age:", age)
+print("home town:", homeTown)
+
+#sep determines the text bettwen next arguments in print
+#end determines the text on the end of the print
