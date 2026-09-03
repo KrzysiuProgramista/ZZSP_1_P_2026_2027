89d915cec1891633a54bef02a8a9dba39c89115a
AdamWojech <wojech.adam@gmail.com>
2026-09-03 16:11:31 +0200

    Add files via upload

---
+++ WOJECH_ADAM/03_09_2026/about_me.py
@@ -0,0 +1,8 @@
+#end specifies what should be printed at the end of an output
+#sep specifies the separator between multiple values in a single print() call
+name = input("Name?")
+age = input("Age?")
+hometown = input("Home town?")
+print("Name:",name)
+print("Age:",age)
+print("Home town:",hometown)
