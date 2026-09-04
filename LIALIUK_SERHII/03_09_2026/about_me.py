a3ffdf8b86480219c0a750aa04329e6226901e48
Leminure! <142736329+Prodrote@users.noreply.github.com>
2026-09-04 23:09:47 +0200

    Delete LIALIUK_SERHII/03_09_2026/about_me.py

--- LIALIUK_SERHII/03_09_2026/about_me.py
+++
@@ -1,9 +0,0 @@
-Name = input("What is your name: ")
-Age = input("What is your age: ")
-City = input("What is your home town: ")
-
-#
-
-print("Name:", Name)
-print("Age:", Age)
-print("Home town:", City)
