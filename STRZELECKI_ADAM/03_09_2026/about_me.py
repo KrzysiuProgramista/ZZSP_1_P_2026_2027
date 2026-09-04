10a71d64a36e18bd0e24669648cb172b0bb30872
JJ-11b <squadbusters.67292579824@gmail.com>
2026-09-04 17:18:10 +0200

    Create about_me.py for user info collection
    Added a script to collect and display user information.

---
+++ STRZELECKI_ADAM/03_09_2026/about_me.py
@@ -0,0 +1,10 @@
+name=input("What is your name?")
+age=input("What is your age?")
+hometown=input("What is your hometown?")
+
+print("Name: " + name)
+print("Age: " + age)
+print("Hometown: " + hometown)
+
+# Sep(separator) is used to separate the values with a space example: print("Name:", name, "Age:", age, "Hometown:", hometown, sep="-")
+# End(terminator) is used to end the print statement with a specific character example: print("Name:", name, "Age:", age, "Hometown:", hometown, end="!")
