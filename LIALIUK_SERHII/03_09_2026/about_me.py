9e698cd6cf554fcdbd50578b7fa34962c8f807f6
Leminure! <142736329+Prodrote@users.noreply.github.com>
2026-09-04 23:09:22 +0200

    Task done.

---
+++ LIALIUK_SERHII/03_09_2026/about_me.py
@@ -0,0 +1,9 @@
+Name = input("What is your name: ")
+Age = input("What is your age: ")
+City = input("What is your home town: ")
+
+#
+
+print("Name:", Name)
+print("Age:", Age)
+print("Home town:", City)
