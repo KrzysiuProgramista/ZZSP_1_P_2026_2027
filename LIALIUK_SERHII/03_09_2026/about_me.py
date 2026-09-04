f806d0b21307f715e980882401800eaa9f625cf9
Leminure! <142736329+Prodrote@users.noreply.github.com>
2026-09-04 23:10:53 +0200

    Add files via upload

---
+++ LIALIUK_SERHII/03_09_2026/about_me.py
@@ -0,0 +1,10 @@
+Name = input("What is your name: ")
+Age = input("What is your age: ")
+City = input("What is your home town: ")
+
+#The "sep" parameter works with the separator used between multiple values in a single "print()" line
+#The "end" parameter works as specifying the character printed at the end of the "print()" output
+
+print("Name:", Name)
+print("Age:", Age)
+print("Home town:", City)
