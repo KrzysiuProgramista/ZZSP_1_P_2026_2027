37cdd52d399327b6e6965a1da52cd7bdd9a670cc
Su1ph <sulph999@gmail.com>
2026-09-05 18:54:54 +0200

    Implement user input for personal information
    Added user input prompts for name, age, and hometown.

--- CZERNIAK_SZYMON/03_09_2026/about_me.py
+++ CZERNIAK_SZYMON/03_09_2026/about_me.py
@@ -1,1 +1,15 @@
+#Those are the lines that ask user for name, age and hometown
 
+name = input('What is your name?:')
+age = int(input('How old are you?:'))
+HomeTown = input('Where do you live?:')
+#The space between the lines
+print()
+
+#That will be the output
+print(f'Name:{name}')
+print(f'Age:{age}')
+print(f'HomeTown:{HomeTown}')
+
+#The sep option seperate the strings with any 
+#The end option decides what to put on the end of print statement
