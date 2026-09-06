67cd47a53b09a85e5aed023e4504fdf79c63fd76
oloolo20100 <aleksanderj1990@gmail.com>
2026-09-06 23:50:58 +0200

    Create about_me.py

---
+++ JEDRZEJCZAK_ALEKSANDER/03_09_2026/about_me.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age =  int(input("What is your age? "))
+hometown = input("What is your home town? ")
+
+print("name: " + name)
+print("age: " , age)
+print("home town: " + hometown)
+
+#Sep – separates values (default is space)
+#Example: print("Name:", name, "Age:", age, sep="-")
+
+#End – ends the print statement with a specific character (default is newline)
+#Example: print("Name:", name, "Age:", age, end="!")
