5498b20a72b7b51065668c083257429ffa1bd087
korcio10 <korciusz@gmail.com>
2026-09-06 21:54:14 +0200

    Update about_me.py

--- FOKS_JAKUB/03_09_2026/about_me.py
+++ FOKS_JAKUB/03_09_2026/about_me.py
@@ -1,1 +1,10 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+town = input("What is your home town? ")
 
+print("Name:", name)
+print("Age:", age)
+print("Home town: ", town)
+
+#sep tells what goes between the values when printed.
+#end tells what goes at the end of the printed line.
