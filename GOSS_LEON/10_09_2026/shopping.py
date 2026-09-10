887584bcb1c2fd58706a9a6b5bc6f3e457192453
leon211111 <gossleon4@gmail.com>
2026-09-10 20:04:55 +0200

    Add shopping script to calculate total cost

---
+++ GOSS_LEON/10_09_2026/shopping.py
@@ -0,0 +1,9 @@
+
+product_name = input("product name: ")
+unit_price = float(input("unit price: "))
+quantity = int(input("quantity: "))
+
+total_cost = unit_price * quantity
+
+
+print(f"{quantity} * {product_name} = {total_cost} PLN")
