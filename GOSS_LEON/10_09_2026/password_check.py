b5fbd8c7ee37c768f73be728e82970ca7a8350a1
leon211111 <gossleon4@gmail.com>
2026-09-10 20:05:35 +0200

    Create password_check.py

---
+++ GOSS_LEON/10_09_2026/password_check.py
@@ -0,0 +1,5 @@
+password = input("enter the password: ")
+length = len(password)
+at_least_8_characters = length >=8
+print(f"length: {length}")
+print(f"at_least_8_characters: {at_least_8_characters}")
