cc30c273d77308e2c4cb4ad78b436b5ab79bcb42
leon211111 <gossleon4@gmail.com>
2026-09-10 20:06:41 +0200

    Clean up temperature.py by removing comment
    Removed unnecessary comment line from temperature.py.

---
+++ GOSS_LEON/10_09_2026/tempreture.py
@@ -0,0 +1,10 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+
+print(f"Temperature in Fahrenheit: {fahrenheit}F")
+print(f"Temperature in Kelvin: {kelvin} K")
+
+# / is float division (returns the exact result as decimal number / float)
+# // is floor divison(rounds the result down to the nearest whole integer)
