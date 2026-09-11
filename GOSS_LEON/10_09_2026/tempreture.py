05269e34f42dcfc75b51fa323babf2c7f736f494
leon211111 <gossleon4@gmail.com>
2026-09-11 22:07:01 +0200

    lekka poprawa komentarzy uproszczenie ich

--- GOSS_LEON/10_09_2026/tempreture.py
+++ GOSS_LEON/10_09_2026/tempreture.py
@@ -6,5 +6,5 @@ kelvin = celsius + 273.15
 print(f"Temperature in Fahrenheit: {fahrenheit}F")
 print(f"Temperature in Kelvin: {kelvin} K")
 
-# / is float division (returns the exact result as decimal number / float)
-# // is floor divison(rounds the result down to the nearest whole integer)
+# / (returns the exact result of division)
+# // (rounds the result down to the nearest whole number)
