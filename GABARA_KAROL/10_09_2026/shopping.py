0de449e286297471041b3266e91d1ce04a2933e7
Ciniminis777 <gaminghackeer321@gmail.com>
2026-09-11 16:18:53 +0200

    Create shopping.py

---
+++ GABARA_KAROL/10_09_2026/shopping.py
@@ -0,0 +1,7 @@
+product = input("Enter product name: ")
+price = float(input("Enter price: "))
+amount = int(input("How many?: "))
+
+cost = price * amount
+
+print(f"{amount} x {product} = {cost} PLN")
