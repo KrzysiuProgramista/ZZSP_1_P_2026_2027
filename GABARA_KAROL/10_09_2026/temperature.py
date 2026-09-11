d8d74e5d92f2b32a16e1f984812f037b55967ffb
Ciniminis777 <gaminghackeer321@gmail.com>
2026-09-11 16:19:53 +0200

    Create temperature.py

---
+++ GABARA_KAROL/10_09_2026/temperature.py
@@ -0,0 +1,10 @@
+celsius = float(input("Enter Celsius temperature: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+kelvin = celsius + 273.15
+
+print(f"Fahrenheit: {fahrenheit}")
+print(f"Kelvin: {kelvin}")
+
+# / returns the exact result of division.
+# // returns the result rounded down to the nearest whole number.
