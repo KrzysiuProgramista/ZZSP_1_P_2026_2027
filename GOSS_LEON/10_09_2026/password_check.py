9da00067569ae0f09763aeccbd77a6b2b214d555
leon211111 <gossleon4@gmail.com>
2026-09-12 14:56:24 +0200

    naprawa kodu źle zinterpretowane zadanie
    Added a check to verify if the password contains at least one numeric character.

--- GOSS_LEON/10_09_2026/password_check.py
+++ GOSS_LEON/10_09_2026/password_check.py
@@ -3,3 +3,5 @@ length = len(password)
 at_least_8_characters = length >=8
 print(f"length: {length}")
 print(f"at_least_8_characters: {at_least_8_characters}")
+if "1" in password or "2" in password or "3" in password or "4" in password or "5" in password or "6" in password or "7" in password or "8" in password or "9" in password or "0" in password:
+    print("The password contains at least one number")
