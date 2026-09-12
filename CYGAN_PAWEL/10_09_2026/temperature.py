b58545d86b0c10e4006d98b4ebbcff0d2ca2cd91
Szefniebobas2 <pawel.m.cygan@gmail.com>
2026-09-12 12:35:14 +0200

    Update temperature.py

--- CYGAN_PAWEL/10_09_2026/temperature.py
+++ CYGAN_PAWEL/10_09_2026/temperature.py
@@ -1,1 +1,7 @@
-
+C = float(input("Enter the temperature in celsius: "))
+F = C * 9/5 + 32
+K = C + 273.15
+print(f"Celsius: {C}°C")
+print(f"Fahrenheit: {F}°F")
+print(f"Kelvin: {K}°K")
+#  The difference between / and // is that a single slash divides the numbers normally while a double slash performs floor division (without the numbers after the dot)
