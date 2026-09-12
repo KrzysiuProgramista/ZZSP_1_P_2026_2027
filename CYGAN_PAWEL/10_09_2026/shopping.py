eb234248ddc52db96092b2dac5b4f50c88f82c28
Szefniebobas2 <pawel.m.cygan@gmail.com>
2026-09-12 12:24:35 +0200

    Update shopping.py

--- CYGAN_PAWEL/10_09_2026/shopping.py
+++ CYGAN_PAWEL/10_09_2026/shopping.py
@@ -1,6 +1,6 @@
 product = input("Enter a product: ")
-price = int(input("Enter the price of the product: "))
+price = float(input("Enter the price of the product: "))
 quantity = int(input("Enter the quantity of the product: "))
 
 calculated_price = quantity * price
-print(quantity , "x" , product, "=" , calculated_price,"$")
+print(quantity , "x" , product, "=" , calculated_price,"PLN")
