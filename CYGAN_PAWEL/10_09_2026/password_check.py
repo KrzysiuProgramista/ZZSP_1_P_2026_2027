431db295e3aea93014748f1ba16a7d5665280ce0
Szefniebobas2 <pawel.m.cygan@gmail.com>
2026-09-12 12:53:21 +0200

    Update password_check.py

--- CYGAN_PAWEL/10_09_2026/password_check.py
+++ CYGAN_PAWEL/10_09_2026/password_check.py
@@ -1,1 +1,6 @@
-
+password = input("Enter your password: ")
+print(len(password))
+if len(password) >= 8: 
+ print("The password has at least 8 characters")
+if "1" in password or "2" in password or "3" in password or "4" in password or "5" in password or "6" in password or "7" in password or "8" in password or "9" in password or "0" in password:
+    print("The password contains at least one digit")
