27839da71f655ff334df5a9e358f49be5342fb87
JJ-11b <squadbusters.67292579824@gmail.com>
2026-09-13 17:19:56 +0200

    Create temperature.py

---
+++ STRZELECKI_ADAM/10_09_2026/temperature.py
@@ -0,0 +1,11 @@
+celcius = float(input("Enter a temperature in Celcius: "))
+farenheit = (celcius * 9/5 + 32)
+kelvin = (celcius + 273.15)
+print("Fahrenheit", farenheit,)
+print("Kelvin", kelvin)
+
+# Difference between / and //
+# A single slash(/) divides numbers and leaves a floating point number.
+result_float = 7/2 # Returns 3.5
+# A double slash(//) divides numbers and rounds them to the nearest full number.
+result_float = 7//2 # Returns 3
