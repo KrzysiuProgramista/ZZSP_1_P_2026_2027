c2f9cf68dd74497fe7abe0c50ba8e6822d91c18c
Su1ph <sulph999@gmail.com>
2026-09-13 17:38:26 +0200

    Implement temperature conversion functionality
    Add temperature conversion from Celsius to Fahrenheit and Kelvin

--- CZERNIAK_SZYMON/10_09_2026/temperature.py
+++ CZERNIAK_SZYMON/10_09_2026/temperature.py
@@ -1,1 +1,6 @@
-
+temperature_in_C = int(input('Temperature in Celcius:')) 
+temperature_in_Fahrenheit = (temperature_in_C * 9/5) + 32 
+temperature_in_Kelvin = temperature_in_C + 273.15
+print(temperature_in_C, 'C') 
+print(temperature_in_Fahrenheit, 'F') 
+print(temperature_in_Kelvin, 'K') 
