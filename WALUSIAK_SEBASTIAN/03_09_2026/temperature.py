eb45c31d4ae6f22623d798c294e2e7c7f4fe3647
Sebsiakk <11sebastianw@gmail.com>
2026-09-15 16:57:29 +0200

    Create temperature.py

---
+++ WALUSIAK_SEBASTIAN/03_09_2026/temperature.py
@@ -0,0 +1,12 @@
+Celsius = input("Podaj temperature w stopniach celsjusza")
+
+Celsius = float(Celsius)
+
+Fahrenheit = Celsius * 9/5 + 32
+Kelvin = Celsius + 273.15
+
+print(f"Fahrenheit = {Fahrenheit}")
+print(f"Kelvin = {Kelvin}")
+
+#/ divides numbers e.g 5/2 = 2.5
+#//also divides numbers but rounds them e.g 5//2 = 2
