ccc3a68c0193313d81c01a0d19b98917082e0c22
leon211111 <gossleon4@gmail.com>
2026-09-17 17:01:36 +0200

    Create matrix.py

---
+++ GOSS_LEON/17_09_2026/matrix.py
@@ -0,0 +1,12 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0])
+print(matrix[1])
+print(matrix[2])
+print("main diagonal:",[matrix[0][0], matrix[1][1], matrix[2][2]])
+
+print(sum(matrix[0]))
