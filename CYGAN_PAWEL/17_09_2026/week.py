47a88abfbbcc392f6fcd0763a01bc7f0104a2c89
Szefniebobas2 <pawel.m.cygan@gmail.com>
2026-09-17 17:01:17 +0200

    Update week.py

--- CYGAN_PAWEL/17_09_2026/week.py
+++ CYGAN_PAWEL/17_09_2026/week.py
@@ -1,1 +1,5 @@
-
+days_of_the_week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+print(days_of_the_week[0:5])
+print(days_of_the_week[5:7])
+print(days_of_the_week[::-1])
+# The pop() method removes an element from a list by index and returns the deleted element, the remove() method deletes the element but it does not return anything, while the del keyword can delete the single value from a list or delete the whole list at a time.
