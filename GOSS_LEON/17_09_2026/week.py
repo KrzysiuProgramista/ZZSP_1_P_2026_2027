b462c552ce9a3bfdb12fe9597c68f83b4cc6cd40
leon211111 <gossleon4@gmail.com>
2026-09-17 17:01:07 +0200

    Create week.py

---
+++ GOSS_LEON/17_09_2026/week.py
@@ -0,0 +1,7 @@
+week = ["monday", "tuesday", "wensday","thursday", "friday", "saturday", "sunday"]
+print(week [0:5])
+print(week[5:7])
+print(week[::-1])
+#remove removes a first occurence of a specific value
+#remove an item from a list a specific position (index) and return that item
+#del remove items from a list by their index or slice or to delete entirevariables
