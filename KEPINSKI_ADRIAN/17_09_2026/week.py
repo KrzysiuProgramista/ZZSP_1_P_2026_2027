9c9f9f13796575673574ecb45a254eb97253e2ae
adrian555f <adrian67ok89@gmail.com>
2026-09-19 15:01:16 +0200

    Create week.py to manage week days
    Added functionality to display working days, weekend days, and reversed week.

---
+++ KEPINSKI_ADRIAN/17_09_2026/week.py
@@ -0,0 +1,11 @@
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+working_days = week[0:5]
+weekend_days = week[5:7]
+print("Working days:", working_days)
+print("Weekend days:", weekend_days)
+print("Reversed week:", week[::-1])
+
+# 3. Explain in a comment the difference between remove(), pop() and del.
+# remove() deletes a value from the list
+# pop() deletes an item using its index and gives the item back
+# del deletes an item or many items using their index
