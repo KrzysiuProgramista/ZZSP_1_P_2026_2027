11dc0e75857119e4b28c2451509b5f047475fc98
adrian555f <adrian67ok89@gmail.com>
2026-09-19 15:04:18 +0200

    Add temperature conversion from Celsius to Fahrenheit and Kelvin

---
+++ KEPINSKI_ADRIAN/10_09_2026/temperature.py
@@ -0,0 +1,10 @@
+celsius = float(input("temperature in celsius: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+
+print("fahrenheit:", fahrenheit)
+print("kelvin:", kelvin)
+
+# / does normal division, for example 5 / 2 = 2.5
+# // does whole number division, for example 5 // 2 = 2
