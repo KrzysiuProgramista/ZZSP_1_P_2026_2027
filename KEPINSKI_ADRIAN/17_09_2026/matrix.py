4c1b8c78561bd1a4b711450968a366d45bdaeb1c
adrian555f <adrian67ok89@gmail.com>
2026-09-19 15:00:19 +0200

    Add matrix initialization and print statements
    Initialize a matrix and print its rows and diagonal elements.

---
+++ KEPINSKI_ADRIAN/17_09_2026/matrix.py
@@ -0,0 +1,18 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# row
+print(matrix[0])
+print(matrix[1])
+print(matrix[2])
+
+# diagonal
+print(matrix[0][0])
+print(matrix[1][1])
+print(matrix[2][2])
+
+
+print(matrix[0][0] + matrix[0][1] + matrix[0][2])
