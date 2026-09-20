c65ceb2937eeb6899f023a1e7f52f14e4f4898aa
oloolo20100 <aleksanderj1990@gmail.com>
2026-09-20 08:04:57 +0200

    Create temperature.py

---
+++ JEDRZEJCZAK_ALEKSANDER/10_09_2026/temperature.py
@@ -0,0 +1,15 @@
+celsius = float(input("Temperature in Celsius: "))
+
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+
+print(f"Fahrenheit: {fahrenheit}")
+print(f"Kelvin: {kelvin}")
+
+# Difference between / and //:
+
+# /  → true division (always returns a float)
+# Example: 7 / 2  →  3.5
+
+# // → floor division (returns the integer part, rounded down)
+# Example: 7 // 2  →  3
