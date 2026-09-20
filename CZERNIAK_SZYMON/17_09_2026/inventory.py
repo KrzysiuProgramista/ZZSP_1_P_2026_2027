3c148394e7d02e78a78639199be32769c9da700f
Su1ph <sulph999@gmail.com>
2026-09-20 14:48:09 +0200

    Implement max quantity product finder
    Add functionality to find and print the product with the maximum quantity from a nested list.

--- CZERNIAK_SZYMON/17_09_2026/inventory.py
+++ CZERNIAK_SZYMON/17_09_2026/inventory.py
@@ -1,1 +1,14 @@
+nested_list = [
+    ['apple', 4],
+    ['bread', 2],
+    ['pear', 3],
+    ['orange', 5]
+]
 
+numbers = [nested_list[0][1], nested_list[1][1], nested_list[2][1], nested_list[3][1]]
+
+max_number = max(numbers)
+
+for product in nested_list:
+    if product[1] == max_number:
+        print(product[0])
