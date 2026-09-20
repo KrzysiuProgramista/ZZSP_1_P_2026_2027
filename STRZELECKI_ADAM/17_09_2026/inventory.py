58e3ca228bde7d99d3370599ec5fd129c1d5ca7f
JJ-11b <squadbusters.67292579824@gmail.com>
2026-09-20 21:31:48 +0200

    Create inventory.py

---
+++ STRZELECKI_ADAM/17_09_2026/inventory.py
@@ -0,0 +1,10 @@
+inventory = [
+    ["Apples", 50],
+    ["Bananas", 120],
+    ["Oranges", 75],
+    ["Milk", 30]
+]
+print("Highest quantity:", inventory[1][0])
+inventory.append(["Bread", 40])
+inventory.sort()
+print("Sorted inventory:", inventory)
