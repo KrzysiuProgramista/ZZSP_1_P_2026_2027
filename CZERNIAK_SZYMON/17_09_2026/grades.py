05c26204b923befd557818313da2a5bc5268d660
Su1ph <sulph999@gmail.com>
2026-09-20 14:44:52 +0200

    Add functions to analyze grades in grades.py

--- CZERNIAK_SZYMON/17_09_2026/grades.py
+++ CZERNIAK_SZYMON/17_09_2026/grades.py
@@ -1,1 +1,6 @@
+grades = [1, 2, 3, 4, 5, 6]
 
+print(max(grades))  #Highest grade
+print(min(grades))  #Lowest grade
+print(sum(grades) // len(grades))  #Avarage grade
+print(grades[::-1]) #List of grades from best to worst 
