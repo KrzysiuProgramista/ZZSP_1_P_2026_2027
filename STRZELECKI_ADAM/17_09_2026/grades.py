1a864c67e18212a1f22f0f83b520ed05fbd19389
JJ-11b <squadbusters.67292579824@gmail.com>
2026-09-20 21:30:21 +0200

    Create grades.py

---
+++ STRZELECKI_ADAM/17_09_2026/grades.py
@@ -0,0 +1,9 @@
+grades = [2, 4, 3, 6, 5, 1]
+highest = (max(grades))
+lowest = (min(grades))
+average = (sum(grades)/len(grades))
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
+grades.sort()
+print(grades)
