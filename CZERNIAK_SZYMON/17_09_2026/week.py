990599c6739e061af7387440752045d150ae0883
Su1ph <sulph999@gmail.com>
2026-09-20 14:45:39 +0200

    Add week_days list and print statements for days

--- CZERNIAK_SZYMON/17_09_2026/week.py
+++ CZERNIAK_SZYMON/17_09_2026/week.py
@@ -1,1 +1,5 @@
+week_days = ['Monday', 'Tuesday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+print(week_days[0:5])  #Only working days
+print(week_days[5:7])  #Only weekend
+print(week_days[::-1])  #Reversed weekdays
