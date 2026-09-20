95dc56c816c95f77acc72c3b04563124afcec04e
Ciniminis777 <gaminghackeer321@gmail.com>
2026-09-20 15:22:32 +0200

    Create matrix.py

---
+++ GABARA_KAROL/17_09_2026/matrix.py
@@ -0,0 +1,13 @@
+numbers = [
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18]
+]
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+
+print("Main diagonal:", numbers[0][0], numbers[1][1], numbers[2][2])
+
+print(sum(numbers[0]))
