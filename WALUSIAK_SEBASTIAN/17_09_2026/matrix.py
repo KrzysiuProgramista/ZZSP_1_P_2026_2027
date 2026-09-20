8f516469074619331a76a62d4572a35d2162eb60
Sebsiakk <11sebastianw@gmail.com>
2026-09-20 21:03:51 +0200

    Add matrix.py to display matrix and calculations

---
+++ WALUSIAK_SEBASTIAN/17_09_2026/matrix.py
@@ -0,0 +1,11 @@
+row_1 = [1, 2, 3]
+row_2 = [4, 6, 7]
+row_3 = [8, 9, 0]
+
+print(row_1)
+print(row_2)
+print(row_3)
+
+print("the values on the main diagonal are:", row_1[0], row_2[1], row_3[2])
+first_row_sum = sum(row_1)
+print(f"the sum of the first row is: {first_row_sum}")
