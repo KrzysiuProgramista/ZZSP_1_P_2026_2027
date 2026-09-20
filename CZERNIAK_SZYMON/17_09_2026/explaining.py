3856fc6d97d77deaab696f488f1ea6c536bc8e57
Su1ph <sulph999@gmail.com>
2026-09-20 14:46:30 +0200

    Add explanations for list removal methods
    Added comments explaining the differences between remove(), pop(), and del functions.

--- CZERNIAK_SZYMON/17_09_2026/explaining.py
+++ CZERNIAK_SZYMON/17_09_2026/explaining.py
@@ -1,1 +1,3 @@
-
+#The "remove()" function removes an item only by its value
+#The "pop()" function removes an item by its index and can return the element
+#The "del" removes an item by its index as well as multiple items within a specific range
