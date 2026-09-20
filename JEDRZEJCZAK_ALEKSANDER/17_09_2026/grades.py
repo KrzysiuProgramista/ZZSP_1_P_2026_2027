cd44ab96d278f9b13b9f4cb4937eaba30b08f2b7
oloolo20100 <aleksanderj1990@gmail.com>
2026-09-20 08:00:48 +0200

    Update grades.py

--- JEDRZEJCZAK_ALEKSANDER/17_09_2026/grades.py
+++ JEDRZEJCZAK_ALEKSANDER/17_09_2026/grades.py
@@ -1,1 +1,6 @@
+grades = [85, 92, 78, 95, 88, 70]
 
+print("Najwyższa ocena:", max(grades))
+print("Najniższa ocena:", min(grades))
+print("Średnia ocena:", sum(grades) / len(grades))
+print("Posortowane od najlepszej do najgorszej:", sorted(grades, reverse=True))
