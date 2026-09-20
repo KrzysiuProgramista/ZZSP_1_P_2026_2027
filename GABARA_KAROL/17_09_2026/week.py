d488ef8ef08434492eaa5ae4cca61369ef938668
Ciniminis777 <gaminghackeer321@gmail.com>
2026-09-20 15:21:58 +0200

    Create week.py

---
+++ GABARA_KAROL/17_09_2026/week.py
@@ -0,0 +1,9 @@
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print(days[:5])
+print(days[5:])
+print(days[::-1])
+
+# remove() usuwa pierwszy element o podanej wartości
+# pop() usuwa element o podanym indeksie i go zwraca
+# del usuwa element, zakres elementów albo całą zmienną
