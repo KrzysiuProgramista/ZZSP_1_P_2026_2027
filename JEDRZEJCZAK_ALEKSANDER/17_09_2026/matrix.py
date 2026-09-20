f77fc0fe9394d2df5e865c641e6b1b84dd8a8aec
oloolo20100 <aleksanderj1990@gmail.com>
2026-09-20 08:01:44 +0200

    Create matrix.py

---
+++ JEDRZEJCZAK_ALEKSANDER/17_09_2026/matrix.py
@@ -0,0 +1,12 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0])
+print(matrix[1])
+print(matrix[2])
+
+print("Main diagonal:", matrix[0][0], matrix[1][1], matrix[2][2])
+print("Sum of the first row:", sum(matrix[0]))
