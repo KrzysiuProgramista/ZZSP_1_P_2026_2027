e3f90c78b96f3d4dfc2560d013f1904f8d0f1f2c
JJ-11b <squadbusters.67292579824@gmail.com>
2026-09-20 21:30:55 +0200

    Create week.py to display and manipulate days of the week
    Added a script to display days of the week and their categories.

---
+++ STRZELECKI_ADAM/17_09_2026/week.py
@@ -0,0 +1,8 @@
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print("Working days: ", days[0:5])
+print("The weekend: ", days[5:7])
+print("Days reversed: ", days[::-1])
+
+# remove: Removes by VALUE (first occurrence). Returns nothing.
+# pop: Removes by INDEX and RETURNS the removed item (default: last).
+# del: Keyword. Removes by INDEX, SLICE, or deletes the whole variable.
