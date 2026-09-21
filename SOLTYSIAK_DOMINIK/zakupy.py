2a94e2109cc4e4c8b275278069851a6ab2cf53e0
voxeliked-code <voxeliked@gmail.com>
2026-09-21 19:54:06 +0200

    Delete SOLTYSIAK_DOMINIK/zakupy.py

--- SOLTYSIAK_DOMINIK/zakupy.py
+++
@@ -1,7 +0,0 @@
-product = input("Product name: ")
-price = float(input("Unit price: "))
-quantity = int(input("Quantity: "))
-
-total = price * quantity
-
-print(f"{quantity} x {product} = {total:.2f} PLN")
