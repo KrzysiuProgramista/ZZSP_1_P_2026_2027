9799a30e542775b6772f0bb2b61c81ae5c8a2d18
voxeliked-code <voxeliked@gmail.com>
2026-09-21 19:54:18 +0200

    Delete SOLTYSIAK_DOMINIK/Dominik_Sołtysik17_09_2026 directory

--- SOLTYSIAK_DOMINIK/Dominik_Sołtysik17_09_2026/grades.py
+++
@@ -1,7 +0,0 @@
-grades = [5, 4, 3, 6, 5, 4]
-
-print("Highest:", max(grades))
-print("Lowest:", min(grades))
-print("Average:", sum(grades) / len(grades))
-
-print("Best to worst:", sorted(grades, reverse=True))
